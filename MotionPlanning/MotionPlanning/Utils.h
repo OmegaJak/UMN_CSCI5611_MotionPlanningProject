c12f2a372a0cd24cc9665a156e72fb6fc4d0c003
OmegaJak <jak.kruger@gmail.com>
2019-03-25 12:20:31 -0500

    Fix segment-sphere intersection
    Previously wasn't doing quadratic formula properly. It just so happened to look fine because of the distance constraint on node connections

--- MotionPlanning/MotionPlanning/Utils.h
+++ MotionPlanning/MotionPlanning/Utils.h
@@ -21,6 +21,7 @@ class Utils {
         return (Random() * (max - min)) + min;
     }
 
+    // Returns a random vector whose coordinates are all between -1 and 1 (inclusive)
     static glm::vec3 RandomVector() {
         float x = (Random() * 2.0f) - 1.0f;
         float y = (Random() * 2.0f) - 1.0f;
@@ -29,33 +30,29 @@ class Utils {
         return glm::normalize(glm::vec3(x, y, z));
     }
 
-    static bool SegmentSphereIntersect(glm::vec3 p1, glm::vec3 p2, glm::vec3 sphere_pos, float sphere_radius) {
+    static bool SegmentSphereIntersect(const glm::vec3& p1, const glm::vec3& p2, const glm::vec3& sphere_pos, const float sphere_radius) {
         glm::vec3 d = (p2 - p1);
-        float dist = glm::length(d);
-        d = glm::normalize(d);
+        float lineLength = glm::length(d);
+        d = d / lineLength;
         // Substitute Segment x,y,z into Sphere 3d equation, and solve for t. (Quadratic formula required)
 
-        // if the direction was not normalized, it would look like this: float A = d.x  * d.x + d.y * d.y + d.z * d.z or d.Dot(d);
-        // but since it is normalized, A is equal to 1
+        // if the direction was not normalized, it would look like this: float A = d.x  * d.x + d.y * d.y + d.z * d.z or
+        // d.Dot(d); but since it is normalized, A is equal to 1
 
         // Quadratic formula values:
         glm::vec3 offset =
             p1 - sphere_pos;  // Offset sphere position from origin of segment (v is position of sphere, p is origin of segment)
-        float B = glm::dot(offset, d);
+        float B = 2 * glm::dot(offset, d);
         float C = glm::dot(offset, offset) - sphere_radius * sphere_radius;
-        float disc = B * B - C;
+        float disc = B * B - 4 * C;
         if (disc < 0) {  // if discriminant is less than 0, the segment does not intersect the sphere.
             return false;
         }
-        float sqdisc = sqrt(disc);  // calculate square root of disc once to increase performance
+        float sqDisc = sqrt(disc);  // calculate square root of disc once to increase performance
 
-        float t0 = (-B + sqdisc);  // t0 and t1 are the two points of intersection of a ray starting at the segment's origin.
-        float t1 = (-B - sqdisc);
-        if (t0 < 0 && t1 < 0) return false;  // If both t0, and t1 are negative, the sphere is behind the camera
-        if (t0 < sphere_radius ||
-            t1 < sphere_radius) {  // If t0 or t1 is smaller than the sphere's radius, the segment has intersected the sphere.
-            return true;
-        }
-        return false;
+        float t0 = (-B + sqDisc) / 2.f;  // t0 and t1 are the two points of intersection of a ray starting at the segment's origin.
+        float t1 = (-B - sqDisc) / 2.f;
+        return ((t0 >= 0 && t0 <= lineLength) ||
+                (t1 >= 0 && t1 <= lineLength));  // If either t0 or t1 lies upon the line segment, there was an intersection
     }
 };
